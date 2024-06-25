5bbb480a86db0897997421497f6886e2b61d77fb
revandadityajw <107557794+revandadityajw@users.noreply.github.com>
2024-06-25 16:41:21 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const product = require("./routes/product.route");
 const healthCheck = require("./routes/health-check.route");
-
+const yt = require('./routes/yt');
 const app = express();
 
 // noinspection JSCheckFunctionSignatures
@@ -11,6 +11,7 @@ app.use(express.json({ extended: false }));
 
 app.use("/", healthCheck);
 app.use("/api/product", product);
+app.use("/ytdl", yt)
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
